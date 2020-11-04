714e8d2ea9bd84ed390f6dffcd65468298a193d3
Hutr0 <Hutro-games@yandex.ru>
2020-11-04 17:27:00 +0300

    PlaceModel was added

---
+++ MyPlaces/Main Screen/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Леонид Лукашевич on 04.11.2020.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let places: [Place] = Place.getPlaces()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+        
+        cell.restaurantImage.image = UIImage(named: places[indexPath.row].image)
+        cell.restaurantImage.layer.cornerRadius = (cell.restaurantImage.frame.size.height) / 2
+        
+        cell.restaurantLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        
+        return cell
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Леонид Лукашевич on 04.11.2020.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = ["Балкан Гриль", "Бочка", "Вкусные истории",
+                           "Дастархан", "Индокитай", "Классик",
+                           "Шок", "Bonsai", "Burger Heroes", "Kitchen",
+                           "Love&Life", "Morris Pub", "Sherlock Holmes",
+                           "Speak Easy", "X.O"]
+    
+    static func getPlaces() -> [Place] {
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Москва", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
